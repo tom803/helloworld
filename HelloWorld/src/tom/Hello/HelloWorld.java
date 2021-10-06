16dc00eb4213bc1607aadedbf09009b8d3ff5582
tom803 <tom803@ist-einmalig.de>
2021-10-06 19:11:24 +0200

    HelloWorld-Class executable (psvm)

--- HelloWorld/src/tom/Hello/HelloWorld.java
+++ HelloWorld/src/tom/Hello/HelloWorld.java
@@ -1,9 +1,16 @@
 package tom.Hello;
 
 public class HelloWorld {
+static String hello = "Hello world!";
+
+	public static void main(String[] args) {
+		System.out.println(hello);
+	}
 
 	@Override
 	public String toString() {
-		return "Hello world!";
+		return hello;
 	}
+	
+
 }
